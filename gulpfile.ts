df109a33b30e73cb3ba908e056c2f7b603554a94
naari3 <naari.named@gmail.com>
2022-11-19 06:26:21 +0900

    feat: add publish task but this is not work now

--- gulpfile.ts
+++ gulpfile.ts
@@ -6,4 +6,6 @@ import makeArtifactNames from "./tasks/makeArtifactNames";
 export const build = gulp.series(buildResourcePack);
 export const zip = gulp.series(zipNomifactoryJa);
 
+export const publish = gulp.series(build, zip); // not work, help me
+
 export { zipNomifactoryJa, buildResourcePack, makeArtifactNames };
