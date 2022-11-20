e64bdce8d43bd1beeddbedc16930ecac84009f2e
naari3 <naari.named@gmail.com>
2022-11-21 00:10:13 +0900

    feat: 単語の対応表を作った

---
+++ wordPairList.ts
@@ -0,0 +1,11 @@
+interface WordPairList {
+	[key: string]: string;
+}
+
+export const wordPairList: WordPairList = {
+	material: "素材",
+	dust: "ダスト",
+	tool: "ツール",
+	world: "ワールド",
+	dimension: "ディメンション",
+};
