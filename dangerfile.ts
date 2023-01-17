c9246997bb16480da48434084e12617c83bf69b5
naari3 <naari.named@gmail.com>
2023-01-18 05:31:27 +0900

    ci: check valid formats

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,5 +1,31 @@
 import { message, danger, fail } from "danger";
 
+const validFormatTypes = [
+	"00",
+	"11",
+	"22",
+	"33",
+	"44",
+	"55",
+	"66",
+	"77",
+	"88",
+	"99",
+	"aa",
+	"bb",
+	"cc",
+	"dd",
+	"ee",
+	"ff",
+	"kk",
+	"ll",
+	"mm",
+	"nn",
+	"oo",
+	"rr",
+	"nl",
+];
+
 function render(content: string) {
 	const i = content.indexOf("=");
 	const splits = [content.slice(0, i), content.slice(i + 1)];
@@ -25,15 +51,13 @@ danger.git.modified_files.forEach((file) => {
 					if (change.content.includes(".desc=")) {
 						message(render(change.content), file, change.ln);
 					}
-					if (change.content.includes("</")) {
-						fail("記載ミスがあります。`</`", file, change.ln);
-					}
-					if (change.content.includes("<br>")) {
-						fail("記載ミスがあります。`<br>`", file, change.ln);
-					}
-					if (change.content.includes("<a>")) {
-						fail("記載ミスがあります。`<a>`", file, change.ln);
-					}
+					const maybeFormats = [change.content.matchAll(/<(.[a-z]\\?)>/g)];
+					maybeFormats.forEach((section) => {
+						const formatType = section[1];
+						// https://minecraft.fandom.com/ja/wiki/%E8%A3%85%E9%A3%BE%E3%82%B3%E3%83%BC%E3%83%89
+						if (validFormatTypes.includes(formatType))
+							fail(`不正なタイプのフォーマットが使われています: <${formatType}>`);
+					});
 				}
 			});
 		});
